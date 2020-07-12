7fe2ff10d557fc2f6f0054db8870df794a02456a
forummodi <fjmodi@gmail.com>
2020-07-12 13:26:03 -0400

    Got match rating caluclations to work, have to fix updating it in the system

--- code/DCD Flask Code/app/app.py
+++ code/DCD Flask Code/app/app.py
@@ -82,6 +82,10 @@ def connect(query):
     
 def connectone(query):
     """ Connect to the PostgreSQL database server """
+    
+    
+    print(query)
+    
     conn = None
     try:
         # read connection parameters
@@ -110,6 +114,39 @@ def connectone(query):
     # return the query result from fetchall()
     return rows
     
+def connectnone(query):
+    """ Connect to the PostgreSQL database server """
+    
+    
+    print(query)
+    
+    conn = None
+    try:
+        # read connection parameters
+        params = config()
+ 
+        # connect to the PostgreSQL server
+        print('Connecting to the %s database...' % (params['database']))
+        conn = psycopg2.connect(**params)
+        print('Connected.')
+      
+        # create a cursor
+        cur = conn.cursor()
+        
+        # execute a query using fetchall()
+        cur.execute(query)
+
+       # close the communication with the PostgreSQL
+        cur.close()
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    finally:
+        if conn is not None:
+            conn.close()
+            print('Database connection closed.')
+    # return the query result from fetchall()
+    return 0
+    
  
 # app.py
 
@@ -121,40 +158,57 @@ app = Flask(__name__)
 def form():
     return render_template('login.html')
 
-#db_cursor = db_conn.cursor()
+
 
 # handle form data
 @app.route('/form-handler', methods=['POST'])
 def handle_data():
 
+    #recives username/password from HTML form potential-match.html
     username = request.form['username']
     password = request.form['password']
-    astro = connectone("SELECT sign_name FROM SIGN_REF WHERE sign_number = (SELECT astrological_sign FROM USER_ACCOUNT WHERE fname=\'"+ username + "\')")
-    pers = connectone("SELECT mbti_name FROM MBTI_REF WHERE mbti_number = (SELECT mb_type FROM USER_ACCOUNT WHERE fname=\'"+ username + "\')")
-    user_id = connectone("SELECT user_id FROM USER_ACCOUNT WHERE fname=\'"+ username + "\'")
+    
     user = connect("SELECT * FROM USER_ACCOUNT WHERE fname=\'"+ username + "\' AND pwd= \'" + password + "'")
-    
-    astro_sign = astro[0]
-    pers_type = pers[0]
-    userID = user_id[0]
-    
+    astro = ["invalid"]
+    pers = ["invalid"]
+    user_id =[0]
     
     
-    for PMID in range(20):
-        if (PMID != userID):
-            PM_ASTRO = connectone("SELECT astrological_sign FROM USER_ACCOUNT WHERE user_id=\'"+str(PMID)+"\'")
-	    print("pm_astro"+PM_ASTRO[0])
-#MATCH_RATING = connectone("SELECT "+astro_sign+" FROM ASTRO_COMPAT WHERE astrological_sign=\'"+PM_ASTRO+"\'")
-#print("PM: " + PM_ASTRO + "\n USER_ASTRO" + astro_sign + "\n RATING:" + MATCH_RATING + "\n\n")
-#"UPDATE POTENTIAL_MATCH SET match_rating = (SELECT astrological_sign FROM ASTRO_COMPAT WHERE " + PMID + "=" + astro_sign"
+    if (len(user) != 0):
+        print("Valid user")  
+        astro = connectone("SELECT sign_name FROM SIGN_REF WHERE sign_number = (SELECT astrological_sign FROM USER_ACCOUNT WHERE fname=\'"+ username + "\')")
+        pers = connectone("SELECT mbti_name FROM MBTI_REF WHERE mbti_number = (SELECT mb_type FROM USER_ACCOUNT WHERE fname=\'"+ username + "\')")
+        user_id = connectone("SELECT user_id FROM USER_ACCOUNT WHERE fname=\'"+ username + "\'")
     
+        astro_sign = astro[0]
+        pers_type = pers[0]
+        userID = user_id[0]
     
+    	#loops from 1 to 20
+        for PMID in range(1, 21):
+            if (PMID != userID):
+                
+                #Calculates ASTRO Rating
+                PM_ASTRO = connectone("SELECT astrological_sign FROM USER_ACCOUNT WHERE user_id=\'"+str(PMID)+"\'")
+                ASTRO_MATCH_RATING = connectone("SELECT "+astro_sign+" FROM ASTRO_COMPAT WHERE astrological_sign="+ str(PM_ASTRO[0]))
+                
+                #Calculates MBTI Rating
+                PM_MBTI = connectone("SELECT mb_type FROM USER_ACCOUNT WHERE user_id=\'"+str(PMID)+"\'")
+                MBTI_MATCH_RATING = connectone("SELECT "+pers_type+" FROM MBTI_COMPAT WHERE personality_type="+ str(PM_MBTI[0]))
+                
+                #adds two ratings together
+                MATCH_RATING = MBTI_MATCH_RATING[0] + ASTRO_MATCH_RATING[0]
+	        
+	        #Trying to update the database but deletes entry instead for some reason???
+                #connectnone("UPDATE POTENTIAL_MATCH SET MATCH_RATING =" + str(MATCH_RATING) + " WHERE user_id=\'" + str(userID) + "\' AND pm_id=\'" + str(PMID)+"\'") 
+
+
     if (len(user) == 0): 
     	user = ['Not a valid user']
+    	astro_sign = "invalid"
+    	pers_type = "invalid"
+    	userID = 0
     	
-    	
-    
-    
     return render_template('user-matches.html', username=username, password=password, astro_sign = astro_sign, pers_type = pers_type, user=user)
 
 if __name__ == '__main__':
